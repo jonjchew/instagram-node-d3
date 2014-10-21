df6f598c54618880fecc3aade06d782ccc6f0961
jonjchew <jon.j.chew@gmail.com>
2014-10-20 22:40:34 -0700

    Ensures sockets is initialized only once.

--- routes/ig.js
+++ routes/ig.js
@@ -1,48 +1,43 @@
 var express = require('express');
 var router = express.Router();
 var config = require('config');
-var Instagram = require('instagram-node-lib');
 var request = require("request");
 var instagram = require('../lib/instagram')
 
 router.get('/callback', function(req, res){
-    var handshake =  Instagram.subscriptions.handshake(req, res);
+  var handshake =  instagram.handshake(req, res);
 });
 
 router.post('/callback', function(req, res) {
-    var io = req.app.get('io');
-    var data = req.body;
+  var io = req.app.get('io');
+  var data = req.body;
 
-    instagram.parseUpdateObjects(data, function(url, hashTag) {
-      request(url, function(error, response, body) {
-        jsonBody = JSON.parse(body);
+  instagram.parseUpdateObjects(data, function(url, hashTag) {
+    request(url, function(error, response, body) {
+      jsonBody = JSON.parse(body);
 
-        if (jsonBody.meta != null && jsonBody.meta.code === 200) {
-          var locationPictures = instagram.filterLocationPictures(jsonBody.data);
-          io.sockets.to(hashTag).emit('msg', { posts: locationPictures });
-        }
-      });
+      if (jsonBody.meta != null && jsonBody.meta.code === 200) {
+        var locationPictures = instagram.filterLocationPictures(jsonBody.data);
+        io.sockets.to(hashTag).emit('msg', { posts: locationPictures });
+      }
     });
-    res.end();
+  });
+  res.end();
 });
 
 router.post('/subscribe', function(req, res) {
-    var io = req.app.get('io');
-    var hashTag = req.body.hash_tag;
+  var io = req.app.get('io');
+  var hashTag = req.body.hash_tag;
 
-    instagram.findRecentByHashtag(hashTag, function(data) {
+  instagram.findRecentByHashtag(hashTag, function(data) {
       io.sockets.to(hashTag).emit('msg', { posts: data });
-    });
+  });
 
-    instagram.subscribeByHashtag(hashTag, function(subscriptionId) {
-      if (req.sessionStore.subscriptions === undefined) {
-        req.sessionStore.subscriptionIds = {};
-      }
-      req.sessionStore.subscriptionIds[hashTag] = subscriptionId;
-    });
+  instagram.subscribeByHashtag(hashTag);
 
-    res.writeHead(200);
-    return res.end();
+  res.writeHead(200);
+  return res.end();
+
 });
 
 module.exports = router;
